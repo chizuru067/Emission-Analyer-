3811f8572040abfc944e7a38add14ce2a4518ecf
chizuru067 <elumalai121101@gmail.com>
2026-02-04 13:38:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,50 @@
-const data = [];
+const modesDiv = document.getElementById("modes");
 
 for (let i = 1; i <= 8; i++) {
-  document.getElementById("modes").innerHTML += `
+  modesDiv.innerHTML += `
     <div class="mode">
       <h3>Mode ${i}</h3>
-      <input type="file" accept="image/*">
-      <br><br>
-      O₂ <input id="o2${i}" size="4">
-      CO <input id="co${i}" size="4">
-      NO <input id="no${i}" size="4">
-      NO₂ <input id="no2${i}" size="4">
-      NOx <input id="nox${i}" size="4">
-      CO₂ <input id="co2${i}" size="4">
+
+      <label>Upload Photo:</label><br>
+      <input type="file" accept="image/*"><br><br>
+
+      O₂ <input id="o2_${i}" size="4">
+      CO <input id="co_${i}" size="4">
+      NO <input id="no_${i}" size="4">
+      NO₂ <input id="no2_${i}" size="4">
+      NOx <input id="nox_${i}" size="4">
+      CO₂ <input id="co2_${i}" size="4">
     </div>
   `;
 }
 
 function generateTable() {
-  data.length = 0;
-  for (let i = 1; i <= 8; i++) {
-    data.push({
-      mode: i,
-      o2: o2${i}.value,
-      co: co${i}.value,
-      no: no${i}.value,
-      no2: no2${i}.value,
-      nox: nox${i}.value,
-      co2: co2${i}.value
-    });
-  }
-
   let html = `
     <table>
       <tr>
-        <th>Mode</th><th>O₂</th><th>CO</th><th>NO</th>
-        <th>NO₂</th><th>NOx</th><th>CO₂</th>
+        <th>Mode</th>
+        <th>O₂</th>
+        <th>CO</th>
+        <th>NO</th>
+        <th>NO₂</th>
+        <th>NOx</th>
+        <th>CO₂</th>
       </tr>
   `;
 
-  data.forEach(r => {
+  for (let i = 1; i <= 8; i++) {
     html += `
       <tr>
-        <td>${r.mode}</td>
-        <td>${r.o2}</td>
-        <td>${r.co}</td>
-        <td>${r.no}</td>
-        <td>${r.no2}</td>
-        <td>${r.nox}</td>
-        <td>${r.co2}</td>
+        <td>${i}</td>
+        <td>${document.getElementById("o2_" + i).value}</td>
+        <td>${document.getElementById("co_" + i).value}</td>
+        <td>${document.getElementById("no_" + i).value}</td>
+        <td>${document.getElementById("no2_" + i).value}</td>
+        <td>${document.getElementById("nox_" + i).value}</td>
+        <td>${document.getElementById("co2_" + i).value}</td>
       </tr>
     `;
-  });
+  }
 
   html += "</table>";
   document.getElementById("output").innerHTML = html;
